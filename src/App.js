8e535d28a71b2dddddeb3f6038c24d831f484d16
FelipeContreras7 <felipecontreras95@gmail.com>
2023-03-12 21:32:09 -0300

    destacados

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import NavBar from "./components/navBar/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Slider from './components/Slider/Slider';
+import Destacados from "./components/Destacados/Destacados";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Slider/>
+      <Destacados/>
     </BrowserRouter>
   );
 }
---
+++ src/components/Destacados/Destacados.jsx
@@ -0,0 +1,31 @@
+import "./destacados.css";
+
+function Destacados () {
+  return(
+  <section id="destacados_slider">
+      <div className="card_container">
+
+        <div className="card_info">
+          <h2>A partir de $1.500</h2>
+          <button>Conocer más</button>
+        </div>
+      </div>
+      <div className="card_container">
+
+        <div className="card_info">
+          <h2>A partir de $1.500</h2>
+          <button>Conocer más</button>
+        </div>
+      </div>
+      <div className="card_container">
+
+        <div className="card_info">
+          <h2>A partir de $1.500</h2>
+          <button>Conocer más</button>
+        </div>
+      </div>
+  </section>
+)}
+
+
+export default Destacados;
